036aa7f042503b775f9b70e1edbb6cb966730104
Multifacio <Mfprojects@live.nl>
2019-04-07 19:55:04 +0200

    Added distribution transformers

--- moldel/Moldel.py
+++ moldel/Moldel.py
@@ -1,8 +1,11 @@
+from DistributionTransformers.RoundTransformer import RoundTransformer
 from ExamDistribution.Data.Data import exam_data
 from ExamDistribution.ExamDistribution import ExamDistribution
 from Printers.PiechartPrinter import PiechartPrinter
 
-dis = ExamDistribution(exam_data, 10000, 3)
-res = dis.compute_distribution(18, None)
+dis = ExamDistribution(exam_data, 10000)
+res = dis.compute_distribution(17, None)
+rounder = RoundTransformer()
+res = rounder.transform_distribution(res, precision = 3)
 printer = PiechartPrinter()
 printer.do_print(res)
