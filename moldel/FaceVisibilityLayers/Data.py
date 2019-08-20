db90459dc8f9752b1575668438fecdf89a21b068
Multifacio <Mfprojects@live.nl>
2019-08-20 23:43:14 +0200

    Added Face Visibility data of season 12 upto 19 without 14

---
+++ moldel/FaceVisibilityLayers/Data.py
@@ -0,0 +1,96 @@
+from Candidates import *
+
+SEASON_12 = {
+    1: {Candidates.FRITS_12: 128, Candidates.MAARTEN_12: 37, Candidates.TIM_12: 122, Candidates.MARION_12: 178,
+        Candidates.ANNE_MARIE_12: 84, Candidates.MARIT_12: 29, Candidates.HADEWYCH_12: 71, Candidates.LIESBETH_12: 57,
+        Candidates.DIO_12: 95, Candidates.WILLIAM_12: 123},
+    2: {Candidates.TIM_12: 61, Candidates.MARIT_12: 95, Candidates.LIESBETH_12: 95, Candidates.DIO_12: 131,
+        Candidates.WILLIAM_12: 94, Candidates.FRITS_12: 178, Candidates.ANNE_MARIE_12: 112, Candidates.MAARTEN_12: 121,
+        Candidates.HADEWYCH_12: 51},
+    3: {Candidates.LIESBETH_12: 45, Candidates.FRITS_12: 36, Candidates.WILLIAM_12: 271, Candidates.HADEWYCH_12: 131,
+        Candidates.MARIT_12: 56, Candidates.ANNE_MARIE_12: 144, Candidates.TIM_12: 135, Candidates.MAARTEN_12: 87},
+    4: {Candidates.ANNE_MARIE_12: 200, Candidates.LIESBETH_12: 78, Candidates.HADEWYCH_12: 98, Candidates.FRITS_12: 247,
+        Candidates.WILLIAM_12: 138, Candidates.TIM_12: 54, Candidates.MARIT_12: 73}
+}
+
+SEASON_13 = {
+    1: {Candidates.PAULIEN_13: 335, Candidates.TANIA_13: 486, Candidates.JOEP_13: 328, Candidates.KEES_13: 189,
+        Candidates.TIM_13: 217, Candidates.ZARAYDA_13: 273, Candidates.CAROLIEN_13: 380, Candidates.DANIEL_13: 105,
+        Candidates.JANINE_13: 386, Candidates.EWOUT_13: 313},
+    2: {Candidates.KEES_13: 195, Candidates.TANIA_13: 298, Candidates.JOEP_13: 354, Candidates.CAROLIEN_13: 358,
+        Candidates.JANINE_13: 258, Candidates.ZARAYDA_13: 224, Candidates.DANIEL_13: 81, Candidates.TIM_13: 317,
+        Candidates.PAULIEN_13: 281},
+    3: {Candidates.TIM_13: 301, Candidates.PAULIEN_13: 322, Candidates.ZARAYDA_13: 293, Candidates.JANINE_13: 142,
+        Candidates.DANIEL_13: 101, Candidates.CAROLIEN_13: 306, Candidates.KEES_13: 78, Candidates.TANIA_13: 565},
+    4: {Candidates.CAROLIEN_13: 280, Candidates.PAULIEN_13: 210, Candidates.DANIEL_13: 134, Candidates.ZARAYDA_13: 269,
+        Candidates.TIM_13: 451, Candidates.TANIA_13: 206, Candidates.KEES_13: 232}
+}
+
+SEASON_15 = {
+    1: {Candidates.MARGRIET_15: 118, Candidates.CHRIS_15: 174, Candidates.PIETER_15: 234, Candidates.CAROLINA_15: 125,
+        Candidates.MARTINE_15: 118, Candidates.MARLIJN_15: 213, Candidates.EVELIEN_15: 167, Candidates.RIK_15: 145,
+        Candidates.AJOUAD_15: 111, Candidates.VIKTOR_15: 261},
+    2: {Candidates.CAROLINA_15: 100, Candidates.VIKTOR_15: 66, Candidates.AJOUAD_15: 30, Candidates.MARGRIET_15: 76,
+        Candidates.CHRIS_15: 55, Candidates.EVELIEN_15: 133, Candidates.MARTINE_15: 101, Candidates.RIK_15: 93,
+        Candidates.MARLIJN_15: 189},
+    3: {Candidates.CHRIS_15: 47, Candidates.AJOUAD_15: 44, Candidates.RIK_15: 192, Candidates.MARGRIET_15: 44,
+        Candidates.VIKTOR_15: 122, Candidates.MARTINE_15: 41, Candidates.MARLIJN_15: 178, Candidates.CAROLINA_15: 116},
+    4: {Candidates.MARLIJN_15: 154, Candidates.MARGRIET_15: 83, Candidates.VIKTOR_15: 103, Candidates.RIK_15: 196,
+        Candidates.AJOUAD_15: 42, Candidates.CAROLINA_15: 217, Candidates.CHRIS_15: 38, Candidates.MARTINE_15: 63}
+}
+
+SEASON_16 = {
+    1: {Candidates.ANNEMIEKE_16: 195, Candidates.CECILE_16: 159, Candidates.AIREN_16: 138, Candidates.TAEKE_16: 96,
+        Candidates.REMY_16: 93, Candidates.ELLIE_16: 127, Candidates.MARJOLEIN_16: 89, Candidates.TIM_16: 160,
+        Candidates.KLAAS_16: 259, Candidates.ROP_16: 111},
+    2: {Candidates.KLAAS_16: 128, Candidates.ANNEMIEKE_16: 116, Candidates.CECILE_16: 155, Candidates.TIM_16: 122,
+        Candidates.ELLIE_16: 85, Candidates.ROP_16: 49, Candidates.MARJOLEIN_16: 99, Candidates.TAEKE_16: 38,
+        Candidates.REMY_16: 106, Candidates.AIREN_16: 196},
+    3: {Candidates.TIM_16: 212, Candidates.ANNEMIEKE_16: 234, Candidates.REMY_16: 215, Candidates.ELLIE_16: 148,
+        Candidates.CECILE_16: 134, Candidates.ROP_16: 133, Candidates.KLAAS_16: 168, Candidates.MARJOLEIN_16: 149,
+        Candidates.TAEKE_16: 141},
+    4: {Candidates.TIM_16: 158, Candidates.KLAAS_16: 311, Candidates.MARJOLEIN_16: 105, Candidates.TAEKE_16: 146,
+        Candidates.CECILE_16: 228, Candidates.ELLIE_16: 190, Candidates.ROP_16: 97, Candidates.ANNEMIEKE_16: 338}
+}
+
+SEASON_17 = {
+    1: {Candidates.YVONNE_17: 144, Candidates.IMANUELLE_17: 106, Candidates.VINCENT_17: 391, Candidates.DIEDERIK_17: 196,
+        Candidates.SIGRID_17: 122, Candidates.SANNE_17: 89, Candidates.THOMAS_17: 119, Candidates.JOCHEM_17: 80,
+        Candidates.ROOS_17: 297, Candidates.JEROEN_17: 99},
+    2: {Candidates.YVONNE_17: 71, Candidates.IMANUELLE_17: 152, Candidates.DIEDERIK_17: 63, Candidates.THOMAS_17: 69,
+        Candidates.SANNE_17: 200, Candidates.ROOS_17: 178, Candidates.JOCHEM_17: 169, Candidates.SIGRID_17: 93,
+        Candidates.JEROEN_17: 34},
+    3: {Candidates.DIEDERIK_17: 200, Candidates.SANNE_17: 104, Candidates.ROOS_17: 271, Candidates.SIGRID_17: 60,
+        Candidates.JEROEN_17: 37, Candidates.IMANUELLE_17: 68, Candidates.THOMAS_17: 144, Candidates.JOCHEM_17: 83},
+    4: {Candidates.THOMAS_17: 74, Candidates.JOCHEM_17: 62, Candidates.DIEDERIK_17: 168, Candidates.IMANUELLE_17: 177,
+        Candidates.JEROEN_17: 214, Candidates.SANNE_17: 106, Candidates.SIGRID_17: 77}
+}
+
+SEASON_18 = {
+    1: {Candidates.EMILIO_18: 99, Candidates.STINE_18: 75, Candidates.JEAN_MARC_18: 138, Candidates.SIMONE_18: 134,
+        Candidates.BELLA_18: 78, Candidates.OLCAY_18: 156, Candidates.JAN_18: 170, Candidates.RON_18: 93,
+        Candidates.RUBEN_18: 66, Candidates.LOES_18: 66},
+    2: {Candidates.RUBEN_18: 212, Candidates.OLCAY_18: 303, Candidates.LOES_18: 165, Candidates.EMILIO_18: 103,
+        Candidates.JEAN_MARC_18: 339, Candidates.STINE_18: 88, Candidates.SIMONE_18: 278, Candidates.BELLA_18: 135,
+        Candidates.JAN_18: 67},
+    3: {Candidates.OLCAY_18: 255, Candidates.BELLA_18: 201, Candidates.RUBEN_18: 189, Candidates.SIMONE_18: 327,
+        Candidates.LOES_18: 156, Candidates.STINE_18: 95, Candidates.JAN_18: 120, Candidates.EMILIO_18: 134},
+    4: {Candidates.OLCAY_18: 279, Candidates.LOES_18: 111, Candidates.EMILIO_18: 286, Candidates.SIMONE_18: 374,
+        Candidates.STINE_18: 129, Candidates.JAN_18: 57, Candidates.RUBEN_18: 183}
+}
+
+SEASON_19 = {
+    1: {Candidates.ROBERT_19: 82, Candidates.RICK_PAUL_19: 173, Candidates.MEREL_19: 136, Candidates.EVI_19: 44,
+        Candidates.NIELS_19: 89, Candidates.SARAH_19: 77, Candidates.SINAN_19: 233, Candidates.EVELIEN_19: 287,
+        Candidates.NIKKIE_19: 182, Candidates.JAMIE_19: 128},
+    2: {Candidates.NIKKIE_19: 215, Candidates.RICK_PAUL_19: 178, Candidates.ROBERT_19: 67, Candidates.EVI_19: 43,
+        Candidates.EVELIEN_19: 194, Candidates.JAMIE_19: 72, Candidates.SINAN_19: 406, Candidates.MEREL_19: 63,
+        Candidates.SARAH_19: 54, Candidates.NIELS_19: 86},
+    3: {Candidates.JAMIE_19: 132, Candidates.ROBERT_19: 126, Candidates.MEREL_19: 122, Candidates.SINAN_19: 194,
+        Candidates.RICK_PAUL_19: 132, Candidates.EVELIEN_19: 277, Candidates.NIKKIE_19: 107, Candidates.SARAH_19: 144,
+        Candidates.NIELS_19: 51},
+    4: {Candidates.SINAN_19: 219, Candidates.JAMIE_19: 89, Candidates.RICK_PAUL_19: 317, Candidates.ROBERT_19: 40,
+        Candidates.SARAH_19: 78, Candidates.MEREL_19: 113, Candidates.NIELS_19: 79, Candidates.EVELIEN_19: 183}
+}
+
+DATA = {12: SEASON_12, 13: SEASON_13, 15: SEASON_15, 16: SEASON_16, 17: SEASON_17, 18: SEASON_18, 19: SEASON_19}
