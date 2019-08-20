b99e98cd22bd36e4a21226a185626aa6ad768394
Multifacio <Mfprojects@live.nl>
2019-08-20 23:49:13 +0200

    Added Identifier Name and Progress print to Face Tracker

--- moldel/FaceVisibilityLayers/FaceTracker.py
+++ moldel/FaceVisibilityLayers/FaceTracker.py
@@ -1,32 +1,32 @@
-from FaceVisibilityLayers.Configuration import FACE_IMAGE_LOCATIONS, EPISODE_VIDEO_LOCATION
+from FaceVisibilityLayers.Configuration import FACE_IMAGE_LOCATIONS, EPISODE_VIDEO_LOCATION, FRAME_SKIP, IDENTIFIER_NAME
 import face_recognition as fr
 import cv2
 
 class FaceTracker:
     """ The FaceTracker determines how often a face of a candidate is visible during an episode. This code is based
-    on the project of mattijn: https://github.com/mattijn/widm """
-
-    # How many frames get skipped before analysing a frame (setting this value higher will make the script run faster,
-    # but makes the results less accurate)
-    FRAME_SKIP = 15
+    on the project of mattijn: https://github.com/mattijn/widm
+    Update the configuration file manually before running this code and store the result in the Data file (which will
+    be used by the Face Visibility Layer). """
 
     # Setting the tolerance higher means that faces get detected more earlier
-    TOLERANCE = 0.5
+    TOLERANCE = 0.50
 
     def track_faces(self):
-        numbered_candidates = enumerate(list(FACE_IMAGE_LOCATIONS.keys()))
+        print(IDENTIFIER_NAME)
+        all_candidates = list(FACE_IMAGE_LOCATIONS.keys())
         score = dict()
-        for _, candidate in numbered_candidates:
+        for candidate in all_candidates:
             score[candidate] = 0
 
         # Load the face images of all candidates
         known_faces = []
-        for _, candidate in numbered_candidates:
+        for candidate in all_candidates:
             image = fr.load_image_file(FACE_IMAGE_LOCATIONS[candidate])
             known_faces.append(fr.face_encodings(image)[0])
 
         # Read the frames of the episode
         video_capture = cv2.VideoCapture(EPISODE_VIDEO_LOCATION)
+        length = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
         frame_count = 0
         while video_capture.isOpened():
             continues, frame = video_capture.read()
@@ -34,7 +34,9 @@ class FaceTracker:
                 video_capture.release()
                 break
             frame_count += 1
-            if frame_count % self.FRAME_SKIP == 0: # Only analyse every FRAME_SKIP frame
+            if frame_count % 500 == 0: # Show progress every 500 frames
+                print('{}/{}'.format(frame_count, length))
+            if frame_count % FRAME_SKIP == 0: # Only analyse every FRAME_SKIP frame
                 # Find all faces in this frame
                 frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 rgb_frame = frame[:, :, ::-1]
@@ -43,15 +45,13 @@ class FaceTracker:
                 for ff in found_faces:
                     # Check which candidate face matches with the founded face
                     match = fr.compare_faces(known_faces, ff, tolerance = self.TOLERANCE)
-                    for i, candidate in numbered_candidates:
+                    for i, candidate in enumerate(all_candidates):
                         if match[i]:
                             score[candidate] += 1
                             break
 
-        # Adjust and print the score
-        score_sum = sum(score.values())
-        for candidate, score in score.items():
-            score[candidate] = score / score_sum
+        # Print the score
+        print(IDENTIFIER_NAME)
         print(score)
 
 ft = FaceTracker()
